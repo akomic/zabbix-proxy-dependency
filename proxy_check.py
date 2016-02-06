9f7798a472ba55f81ad319f3399dd7f0cb40393e
akomic <akomic@gmail.com>
2016-02-06 17:13:22 +0100

    README

---
+++ proxy_check.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+"""
+This program takes as required input zabbix hostid and returns the health status of the zabbix proxy if the host is using it.
+If there is no proxy for the specified host, 1 (available) is going to be returned.
+Idea is to have something the host can depend on if the zabbix proxy goes down in order to avoid avalanche of alerts.
+For this to work, you need to have zabbix agent installed on the zabbix proxy machine and added as regular host for monitoring.
+"""
+
+__author__ = "Alen Komic"
+__license__ = "GPL"
+__version__ = "1.0"
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--hostid",help="Host ID", type=int, required=True)
+parser.add_argument("--username",help="Zabbix Username")
+parser.add_argument("--password",help="Zabbix Password")
+parser.add_argument("--verbose",help="Up the verbosity, for debugging purposes", action="count")
+args = parser.parse_args()
+
+import os
+import sys
+import pyzabbix
+import requests
+
+zapi = pyzabbix.ZabbixAPI("http://localhost/zabbix")
+
+
+zuser = ( args.username or os.getenv('ZABBIX_USER') )
+zpass = ( args.password or os.getenv('ZABBIX_PASS') )
+
+if not zuser or not zpass:
+  print "You must specify zabbix username and password via cli or ENV"
+  sys.exit(1)
+  
+try:
+  zapi.login(zuser,zpass)
+except requests.exceptions.HTTPError as e:
+  print "ERROR login to zabbix API:", str(e)
+except pyzabbix.ZabbixAPIException as e:
+  print "ERROR login to zabbix API:", str(e)
+else:
+  if args.verbose:
+    print "Connected to Zabbix API Version %s" % zapi.api_version()
+
+  # Default is to return 1 (available), even if zabbix proxy does not exist for the specified host.
+  proxy_dep_alive=1
+
+  # Looking for host with given hostid
+  for host in zapi.host.get(hostids=[args.hostid], output="extend", limit=1):
+    if args.verbose:
+      print "Checking host id:",args.hostid,"host:",host['host']
+    # Is there zabbix proxy this host is using?
+    if host['proxy_hostid']:
+      # Getting the proxy hostname
+      for proxy in zapi.proxy.get(proxyids=[host['proxy_hostid']], output="extend", limit=1):
+        if args.verbose:
+          print "Proxy id:",proxy['proxyid'],"proxy host:",proxy['host']
+
+        # Zabbix proxy is represented as regular host, we are counting on it.
+        for proxyhost in zapi.host.get(filter={"host": proxy['host']}, output="extend", limit=1):
+          if not proxyhost.get('available'):
+            proxy_dep_alive=0
+
+  if args.verbose:
+    print "Proxy availability status:",
+  print proxy_dep_alive
